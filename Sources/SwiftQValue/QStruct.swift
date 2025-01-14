7da279cd0d715a8ea003934edeffeeaa73605fc8
Yuma Antoine Decaux <yuma@astreos.space>
2025-01-14 18:25:31 +1000

    INitial coomit + sstruct + enum

---
+++ Sources/SwiftQValue/QStruct.swift
@@ -0,0 +1,61 @@
+//
+//  QStruct.swift
+//  SwiftQValue
+//
+//  Created by Yuma decaux on 14/1/2025.
+//
+
+
+public enum QStruct:Codable {
+    case qValue(QValue)
+    case qArr([QValue])
+    case qDict([[String: QValue]])
+    case qDictSingle([String: QValue])
+    
+    public init(values: Any) {
+        switch values {
+        case let qValue as QValue:
+            self = .qValue(qValue)
+        case let qArr as [QValue]:
+            self = .qArr(qArr)
+        case let qDict as [[String: QValue]]:
+            self = .qDict(qDict)
+        case let qDictSingle as [String: QValue]:
+            self = .qDictSingle(qDictSingle)
+        default:
+            fatalError("Incompatible data structure")
+        }
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let qValue = try? container.decode(QValue.self) {
+            self = .qValue(qValue)
+        }
+        if let qArr = try? container.decode([QValue].self) {
+            self = .qArr(qArr)
+        }
+        if let qDict = try? container.decode([[String:QValue]].self) {
+            self = .qDict(qDict)
+        }
+        if let qDictSingle = try? container.decode([String:QValue].self) {
+            self = .qDictSingle(qDictSingle)
+        }
+            fatalError("Failed to decode FilterValues")
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .qValue(let qValue):
+            try container.encode(qValue)
+        case .qArr(let qArr):
+            try container.encode(qArr)
+        case .qDict(let qDict):
+            try container.encode(qDict)
+        case .qDictSingle(let qDictSingle):
+            try container.encode(qDictSingle)
+        }
+    }
+
+}
---
+++ Sources/SwiftQValue/QValue.swift
@@ -0,0 +1,107 @@
+//
+//  QValue.swift
+//  SwiftQValue
+//
+//  Created by Yuma decaux on 14/1/2025.
+//
+
+
+public enum QValue: Hashable, Identifiable {
+    /** Quantum value which collapses to a working type
+     for codable Json structs
+     */
+    case int(Int)
+    case string(String)
+    case float(Float)
+    case bool(Bool)
+    
+    public var id: Self {
+        return self
+    }
+    
+    public func type() -> String {
+        switch self {
+        case .string(_): return "String"
+                     case .int(_): return
+                     "int"
+                     case .float(_): return
+                     "float"
+                     case .bool(_): return "bool"
+        }
+    }
+}
+extension QValue:Codable {
+
+    public init(from decoder: Decoder) throws {
+        if let intValue = try? decoder.singleValueContainer().decode(Int.self) {
+            self = .int(intValue)
+            return
+        }
+        if let stringValue = try? decoder.singleValueContainer().decode(String.self) {
+            self = .string(stringValue)
+            return
+        }
+        if let floatValue = try? decoder.singleValueContainer().decode(Float.self) {
+            self = .float(floatValue)
+            return
+        }
+        if let boolValue = try? decoder.singleValueContainer().decode(Bool.self) {
+            self = .bool(boolValue)
+            return
+        }
+        
+        // This is a NULL value so keep as empty string
+        // instead of throwing error
+        // The removenullrecord flag in the request
+        // will omit these key/value pairs
+        self = .string("")
+    }
+
+    public init(value: String) {
+        if let int = Int(value) {
+            self = .int(int)
+            return
+        }
+        if let float = Float(value) {
+            self = .float(float)
+            return
+        }
+        if let bool = Bool(value) {
+            self = .bool(bool)
+            return
+        }
+            self = .string(value)
+    }
+
+    
+    public func encode(to encoder: Encoder) throws {
+        var singleContainer = encoder.singleValueContainer()
+        
+        switch self {
+        case .int(let int):
+            try singleContainer.encode(int)
+        case .string(let string):
+            try singleContainer.encode(string)
+        case .float(let float):
+            try singleContainer.encode(float)
+        case .bool(let bool):
+            try singleContainer.encode(bool)
+        }
+    }
+
+    public var value:Any {
+        switch self {
+        case .string(let str):
+            return str
+        case .float(let ft):
+            return ft
+        case .bool(let b):
+            return b
+        case .int(let n):
+            return n
+        }
+    }
+    
+    
+}
+
